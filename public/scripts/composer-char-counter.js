904ca5a3a936b11bc94148ef636d6cbebca97830
Melissa <mawilliamson10@gmail.com>
2022-02-02 00:52:28 +0000

    update counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-  $('.new-tweet )
-
-  
+  $('#tweet-text').on("input", function () {
+    let newTweet = $(this).val().length;
+    let characterLeft = 140 - newTweet;
+    let counterElement = $(this).next().find(".counter");
+    counterElement.text(140 - newTweet);
+    if (characterLeft < 0) {
+      counterElement.addClass("color-red");
+    } else {
+      counterElement.removeClass("color-red");
+    }
+  })
 });
