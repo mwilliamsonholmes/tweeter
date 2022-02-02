fbf6520f903f4a5e3821f06524e5e98818551eab
Melissa <mawilliamson10@gmail.com>
2022-02-02 23:16:07 +0000

    add event listener for submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,17 +3,18 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-
 //given a tweet object
 // create JS function that geneartes DOM structure for a tweet
 //document ready function- code inside here will only run once the page(DOM) is ready for JavaScript code to execute
-$(document).ready(function () {
 
-  const createTweetElement = function (tweetObj) {
-    const $tweet = $("<article>").addClass("tweet");
-    const markup = `
-<header>
+$(document).ready(function () {
+  loadTweets();
+});
+
+const createTweetElement = function (tweetObj) {
+  const $tweet = $("<article>").addClass("tweet");
+  const markup = `
+<header class="header">
 <img src = ${tweetObj.user.avatars}>
 <span> ${tweetObj.user.name}></span>
 <span> ${tweetObj.user.handle}></span>
@@ -21,23 +22,36 @@ $(document).ready(function () {
 <span>${tweetObj.content.text}</span>
 
 <footer>
-<span>${timeago.format(created_at)}></span>
+<span>${timeago.format(tweetData.created_at)}></span>
 <span class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-regular fa-heart"></i></span>
 </footer>`;
-    $tweet.append(markup);
-    return $tweet;
-  }
-});
+  let tweetElement = $tweet.append(markup)
+  return tweetElement;
+
+}
+// });
 
 const renderTweets = function (tweets) {
+  //takes in an array of tweet objects
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $("section.all-tweets").append($tweet);
-  }
+  let tweetContainer = $('.all-tweets');
+
+  tweets.forEach((tweet) => {
+    let tweetElement = createTweetElement(tweet);
+    tweetContainer.prepend(tweetElement)
+  })
 };
+
+
+const loadTweets = function () {
+  $.get('/tweets')
+    .then(data => {
+      renderTweets(data);
+    })
+}
+
 
 
 const tweetData = {
@@ -52,8 +66,3 @@ const tweetData = {
   "created_at": 1461116232227
 }
 
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.all-tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
