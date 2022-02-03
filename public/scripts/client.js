ba5ac049b6f87a3ddaaee765db2a307472a050bb
Melissa <mawilliamson10@gmail.com>
2022-02-03 03:16:29 +0000

    added error message for empty tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,12 +8,9 @@
 //document ready function- code inside here will only run once the page(DOM) is ready for JavaScript code to execute
 
 $(document).ready(function () {
-  loadTweets();
-});
-
-const createTweetElement = function (tweetObj) {
-  const $tweet = $("<article>").addClass("tweet");
-  const markup = `
+  const createTweetElement = function (tweetObj) {
+    const $tweet = $("<article>").addClass("tweet");
+    const markup = `
 <header class="header">
 <img src = ${tweetObj.user.avatars}>
 <span>${tweetObj.user.name}</span>
@@ -25,50 +22,58 @@ const createTweetElement = function (tweetObj) {
 <span>${timeago.format(tweetObj.created_at)}></span>
 <span class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-regular fa-heart"></i></span>
 </footer>`;
-  let tweetElement = $tweet.append(markup)
-  return tweetElement;
+    let tweetElement = $tweet.append(markup)
+    return tweetElement;
 
-}
-// });
+  }
+  // });
 
-const renderTweets = function (tweets) {
-  //takes in an array of tweet objects
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  let tweetContainer = $('.all-tweets');
+  const renderTweets = function (tweets) {
+    //takes in an array of tweet objects
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    let tweetContainer = $('.all-tweets');
 
-  tweets.forEach((tweet) => {
-    let tweetElement = createTweetElement(tweet);
-    tweetContainer.prepend(tweetElement)
-  })
-};
-
-//get tweets
-const loadTweets = function () {
-  $.ajax('/tweets', { method: "GET", dataType: "json" })
-    .then(function (allTweets) {
-      renderTweets(allTweets);
+    tweets.forEach((tweet) => {
+      let tweetElement = createTweetElement(tweet);
+      tweetContainer.prepend(tweetElement)
     })
-}
-loadTweets();
-// tweetContainer.empty();
+  };
 
-//send data to server ajax
-$('#form').on('submit', function (event) {
-  event.preventDefault();
-  console.log("hello");
-  $.ajax({
-    method: "POST",
-    url: $("#form").attr("action"),
-    data: $("#form").serialize(),
-    success: () => loadTweets(),
+  //get tweets
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: "GET", dataType: "json" })
+      .then(function (allTweets) {
+        renderTweets(allTweets);
+      })
+  }
+  loadTweets();
+  // tweetContainer.empty();
+
+  //send data to server ajax
+  $('#form').on('submit', function (event) {
+    event.preventDefault();
+    let tweetLength = $("#tweet-text").val();
+    if (tweetLength === "" || tweetLength === null) {
+      alert("Tweet cannot be empty.");
+      return;
+    }
+    if (tweetLength.length > 140) {
+      alert(`Tweet cannot be more than 140 characters- you have ${tweetLength}.`);
+      return;
+    }
+    $.ajax({
+      method: "POST",
+      url: $("#form").attr("action"),
+      data: $("#form").serialize(),
+      success: () => loadTweets(),
+    })
   })
-})
 
 
-
-
+  loadTweets();
+});
 
 // const tweetData = {
 //   "user": {
